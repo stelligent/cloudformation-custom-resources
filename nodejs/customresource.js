6d6d140efa451b607623e2e087b7dc4cb2520e26
jonny sywulak <jonny@stelligent.com>
2017-11-16 21:40:53 -0500

    first cut, creates custom resource handler

---
+++ nodejs/customresource.js
@@ -0,0 +1,77 @@
+var aws = require("aws-sdk");
+ 
+exports.handler = function(event, context) {
+    console.log("REQUEST RECEIVED:\n" + JSON.stringify(event));
+    console.log("Hi Jonny!")
+    // For Delete requests, immediately send a SUCCESS response.
+    if (event.RequestType == "Create") {
+        console.log("CREATE!")
+        sendResponse(event, context, "SUCCESS");
+        return;
+    }
+     // For Delete requests, immediately send a SUCCESS response.
+    else if (event.RequestType == "Update") {
+        console.log("UDPATE!")
+        sendResponse(event, context, "SUCCESS");
+        return;
+    }
+    // For Delete requests, immediately send a SUCCESS response.
+    else if (event.RequestType == "Delete") {
+        console.log("DELETE!")
+        sendResponse(event, context, "SUCCESS");
+        return;
+    } else {
+        console.log("FAILURE!")
+        sendResponse(event, context, "FAILURE");
+    }
+};
+
+// Send response to the pre-signed S3 URL 
+function sendResponse(event, context, responseStatus, responseData) {
+    console.log("Sending response " + responseStatus)
+    var responseBody = JSON.stringify({
+        Status: responseStatus,
+        Reason: "See the details in CloudWatch Log Stream: " + context.logStreamName,
+        PhysicalResourceId: context.logStreamName,
+        StackId: event.StackId,
+        RequestId: event.RequestId,
+        LogicalResourceId: event.LogicalResourceId,
+        Data: responseData
+    });
+ 
+    console.log("RESPONSE BODY:\n", responseBody);
+ 
+    var https = require("https");
+    var url = require("url");
+ 
+    var parsedUrl = url.parse(event.ResponseURL);
+    var options = {
+        hostname: parsedUrl.hostname,
+        port: 443,
+        path: parsedUrl.path,
+        method: "PUT",
+        headers: {
+            "content-type": "",
+            "content-length": responseBody.length
+        }
+    };
+ 
+    console.log("SENDING RESPONSE...\n");
+ 
+    var request = https.request(options, function(response) {
+        console.log("STATUS: " + response.statusCode);
+        console.log("HEADERS: " + JSON.stringify(response.headers));
+        // Tell AWS Lambda that the function execution is done  
+        context.done();
+    });
+ 
+    request.on("error", function(error) {
+        console.log("sendResponse Error:" + error);
+        // Tell AWS Lambda that the function execution is done  
+        context.done();
+    });
+  
+    // write data to request body
+    request.write(responseBody);
+    request.end();
+}
