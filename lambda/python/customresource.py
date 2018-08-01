791b8e0bc7fe993e2d5637b8efea92f464a130db
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2018-08-01 13:26:12 -0400

    Lint

--- lambda/python/customresource.py
+++ lambda/python/customresource.py
@@ -18,48 +18,49 @@ def handler(event, context):
         LOGGER.info('REQUEST RECEIVED:\n %s', context)
         if event['RequestType'] == 'Create':
             LOGGER.info('CREATE!')
-            sendResponse(event, context, "SUCCESS",
-                         {"Message": "Resource creation successful!"})
+            send_response(event, context, "SUCCESS",
+                          {"Message": "Resource creation successful!"})
         elif event['RequestType'] == 'Update':
             LOGGER.info('UPDATE!')
-            sendResponse(event, context, "SUCCESS",
-                         {"Message": "Resource update successful!"})
+            send_response(event, context, "SUCCESS",
+                          {"Message": "Resource update successful!"})
         elif event['RequestType'] == 'Delete':
             LOGGER.info('DELETE!')
-            sendResponse(event, context, "SUCCESS",
-                         {"Message": "Resource deletion successful!"})
+            send_response(event, context, "SUCCESS",
+                          {"Message": "Resource deletion successful!"})
         else:
             LOGGER.info('FAILED!')
-            sendResponse(event, context, "FAILED",
-                         {"Message": "Unexpected event received from CloudFormation"})
+            send_response(event, context, "FAILED",
+                          {"Message": "Unexpected event received from CloudFormation"})
     except:
         LOGGER.info('FAILED!')
-        sendResponse(event, context, "FAILED", {
-                     "Message": "Exception during processing"})
+        send_response(event, context, "FAILED", {
+            "Message": "Exception during processing"})
 
 
-def sendResponse(event, context, responseStatus, responseData):
-    responseBody = json.dumps({
-        "Status": responseStatus,
+def send_response(event, context, response_status, response_data):
+    '''Send a resource manipulation status response to CloudFormation'''
+    response_body = json.dumps({
+        "Status": response_status,
         "Reason": "See the details in CloudWatch Log Stream: " + context.log_stream_name,
         "PhysicalResourceId": context.log_stream_name,
         "StackId": event['StackId'],
         "RequestId": event['RequestId'],
         "LogicalResourceId": event['LogicalResourceId'],
-        "Data": responseData
+        "Data": response_data
     })
 
     LOGGER.info('ResponseURL: %s', event['ResponseURL'])
-    LOGGER.info('ResponseBody: %s', responseBody)
+    LOGGER.info('ResponseBody: %s', response_body)
 
     opener = build_opener(HTTPHandler)
-    request = Request(event['ResponseURL'], data=responseBody)
+    request = Request(event['ResponseURL'], data=response_body)
     request.add_header('Content-Type', '')
-    request.add_header('Content-Length', len(responseBody))
+    request.add_header('Content-Length', len(response_body))
     request.get_method = lambda: 'PUT'
     response = opener.open(request)
-    print("Status code: {}".format(response.getcode()))
-    print("Status message: {}".format(response.msg))
+    LOGGER.info("Status code: %s", response.getcode())
+    LOGGER.info("Status message: %s", response.msg)
 
 
 def timeout_handler(_signal, _frame):
